835a7d67ef37ae41ffd1ca2e018b1d7da098f2d1
Oleg Udanov <oleg.udanov@gmail.com>
2023-05-22 13:51:49 +0700

    Mixing the numbers on the board

--- board.c
+++ board.c
@@ -15,3 +15,33 @@ void initialize_board(){
     }
     board[empty_row][empty_col] = 0;
 }
+
+void mixing_board(){
+    srand(time(Null));
+    for (int row = 0; row < 100; row++){
+        int movement = rand() % 5 + 1;
+        switch (movement){
+            case 1: //влево
+                if (empty_col > 0) {
+                    board[empty_row][empty_col] = board[empty_row][empty_col - 1];
+                    board[empty_row][empty_col - 1] = 0;
+                }
+            case 2: //вверх
+                if (empty_row > 0) {
+                    board[empty_row][empty_col] = board[empty_row + 1][empty_col];
+                    board[empty_row + 1][empty_col] = 0;
+                }
+            case 3: //вправо
+                if (empty_col > SIZE - 1) {
+                    board[empty_row][empty_col] = board[empty_row][empty_col + 1];
+                    board[empty_row][empty_col + 1] = 0;
+                }
+            case 4: //вниз
+                if (empty_row > SIZE - 1) {
+                    board[empty_row][empty_col] = board[empty_row - 1][empty_col];
+                    board[empty_row - 1][empty_col] = 0;
+                }
+                break;
+        }
+    }
+}
