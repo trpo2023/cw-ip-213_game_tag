7877091d076a4527f2697b7ab1dd2a8822140b8d
Varaksin Ivan <VanyaVAR@users.noreply.github.com>
2023-06-02 11:40:22 +0700

    Verification function

--- board.c
+++ board.c
@@ -7,56 +7,72 @@
 int board[SIZE][SIZE];
 int empty_row, empty_col;
 
-void initialize_board(){
+void initialize_board()
+{
     int num = 1;
-    for (int row = 0; row < SIZE; row++){
-        for (int col = 0; col < SIZE; col++){
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
             board[row][col] = num++;
         }
     }
     board[empty_row][empty_col] = 0;
 }
 
-void mixing_board(){
+void mixing_board()
+{
     srand(time(Null));
-    for (int row = 0; row < 100; row++){
+    for (int row = 0; row < 100; row++) {
         int movement = rand() % 5 + 1;
-        switch (movement){
-            case 1: //влево
-                if (empty_col > 0) {
-                    board[empty_row][empty_col] = board[empty_row][empty_col - 1];
-                    board[empty_row][empty_col - 1] = 0;
-                }
-            case 2: //вверх
-                if (empty_row > 0) {
-                    board[empty_row][empty_col] = board[empty_row + 1][empty_col];
-                    board[empty_row + 1][empty_col] = 0;
-                }
-            case 3: //вправо
-                if (empty_col > SIZE - 1) {
-                    board[empty_row][empty_col] = board[empty_row][empty_col + 1];
-                    board[empty_row][empty_col + 1] = 0;
-                }
-            case 4: //вниз
-                if (empty_row > SIZE - 1) {
-                    board[empty_row][empty_col] = board[empty_row - 1][empty_col];
-                    board[empty_row - 1][empty_col] = 0;
-                }
-                break;
+        switch (movement) {
+        case 1: // влево
+            if (empty_col > 0) {
+                board[empty_row][empty_col] = board[empty_row][empty_col - 1];
+                board[empty_row][empty_col - 1] = 0;
+            }
+        case 2: // вверх
+            if (empty_row > 0) {
+                board[empty_row][empty_col] = board[empty_row + 1][empty_col];
+                board[empty_row + 1][empty_col] = 0;
+            }
+        case 3: // вправо
+            if (empty_col > SIZE - 1) {
+                board[empty_row][empty_col] = board[empty_row][empty_col + 1];
+                board[empty_row][empty_col + 1] = 0;
+            }
+        case 4: // вниз
+            if (empty_row > SIZE - 1) {
+                board[empty_row][empty_col] = board[empty_row - 1][empty_col];
+                board[empty_row - 1][empty_col] = 0;
+            }
+            break;
         }
     }
 }
 
-void print_board(){
-    for (int row = 0; row < SIZE; row++){
-        for (int col = 0; col < SIZE; row++){
-            if (board[row][col] == 0){
+void print_board()
+{
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; row++) {
+            if (board[row][col] == 0) {
                 printf("  ");
-            } else{
+            } else {
                 printf("%2d", board[row][col]);
             }
             printf(" ")
         }
         printf("\n");
     }
+}
+
+int is_solved()
+{
+    int value = 1;
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            if (board[row][col] != value++) {
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
