acd13ac2c9843a33a78979907e0b6090bcc67251
Varaksin Ivan <VanyaVAR@users.noreply.github.com>
2023-06-03 14:57:30 +0700

    Error work in board.c

--- board.c
+++ board.c
@@ -24,39 +24,44 @@ void initialize_board()
 
 void mixing_board()
 {
-    srand(time(Null));
+    srand(time(NULL));
     for (int row = 0; row < 100; row++) {
         int movement = rand() % 4 + 1;
         switch (movement) {
         case 1: // влево
             if (empty_col > 0) {
                 board[empty_row][empty_col] = board[empty_row][empty_col - 1];
-                board[empty_row][empty_col - 1] = 0;
+                board[empty_row][--empty_col] = 0;
             }
+            break;
         case 2: // вверх
             if (empty_row > 0) {
                 board[empty_row][empty_col] = board[empty_row - 1][empty_col];
-                board[empty_row - 1][empty_col] = 0;
+                board[--empty_row][empty_col] = 0;
             }
+            break;
         case 3: // вправо
-            if (empty_col > SIZE - 1) {
+            if (empty_col < SIZE - 1) {
                 board[empty_row][empty_col] = board[empty_row][empty_col + 1];
-                board[empty_row][empty_col + 1] = 0;
+                board[empty_row][++empty_col] = 0;
             }
+            break;
         case 4: // вниз
-            if (empty_row > SIZE - 1) {
+            if (empty_row < SIZE - 1) {
                 board[empty_row][empty_col] = board[empty_row + 1][empty_col];
-                board[empty_row + 1][empty_col] = 0;
+                board[++empty_row][empty_col] = 0;
             }
             break;
         }
     }
 }
 
+
+
 void print_board()
 {
     for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
             if (board[row][col] == 0) {
                 printf("  ");
             } else {
