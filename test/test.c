9eb878f0dc91d07946b6b93b330a7d9d04e1acaa
Oleg Udanov <Oleg257@users.noreply.github.com>
2023-06-26 21:06:40 +0700

    Modified test.c

--- test/test.c
+++ test/test.c
@@ -1,7 +1,7 @@
 #include "../thirdparty/ctest.h"
 #include "../src/board/board.h"
 
-CTEST(board_suite, ctest_board_is_solved_test)
+CTEST(board_suite, ctest_initialize_board_test)
 {
     initialize_board();
 
@@ -16,4 +16,21 @@ CTEST(board_suite, ctest_board_is_solved_test)
     ASSERT_TRUE(is_solved());
 
     ASSERT_TRUE(1);
+}
+
+CTEST(board_suite, ctest_mixing_board_test) 
+{
+    mixing_board();
+
+    int count[SIZE * SIZE] = {0};
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            int num = board[row][col];
+            count[num]++;
+            ASSERT_TRUE(num >= 0 && num < SIZE * SIZE);
+        }
+    }
+    for (int i = 0; i < SIZE * SIZE; i++) {
+        ASSERT_TRUE(count[i] == 1);
+    }
 }
